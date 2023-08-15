e394ff254d1e7200951e8f5cf18c9b6731dbdadd
khawla <khawlabensalahwork@gmail.com>
2023-08-15 12:40:39 -0700

    kkk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: khawlabensalah
+ */
+
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ *
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
+ */
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
